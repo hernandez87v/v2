5aba0139474d8e4f423262c61135ec83e0ce40f6
hernandez87v <hernandez87v@hotmail.com>
2020-10-28 18:12:04 -0700

    add navbar items 🧭

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <div>
-      <h1>this is NAV</h1>
-    </div>
+    <header id="navbar-header">
+      <h2 className="logo" alt="logo">
+        <Link to="/">Vlad Hernandez</Link>
+      </h2>
+      <nav>
+        <ul className="nav_links">
+          <li>
+            <Link to="/placeholder">placeholder</Link>
+          </li>
+        </ul>
+      </nav>
+      <a className="cta" href="mailto:hernandez87v@hotmail.com">
+        <button>Contact</button>
+      </a>
+    </header>
   );
 }
