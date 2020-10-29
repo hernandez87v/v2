2ce6145bbd0169bb55565e3e796d5f84f34cf177
hernandez87v <hernandez87v@hotmail.com>
2020-10-29 13:45:04 -0700

    add react-three-fiber dependancies for Home 3D Scene 🖼

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Home.css';
+
+import { Canvas } from 'react-three-fiber';
+import * as THREE from 'three';
+import Coolvetica from './Coolvetica.json';
 
 export default function Home() {
+  // parse JSON file with Three
+  const font = new THREE.FontLoader().parse(Coolvetica);
+
+  // configure font geometry
+  const textOptions = {
+    font,
+    size: 2,
+    height: 1,
+  };
   return (
-    <div>
-      <h1>this is Home</h1>
+    <div className="Home">
+      <Canvas
+        style={{ height: '100vh', width: '100vw' }} // stretch the canvas to the full viewport size
+      >
+        <mesh>
+          <textGeometry attach="geometry" args={['Welcome', textOptions]} />
+          <meshStandardMaterial attach="material" />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
