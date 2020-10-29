99b72f69504de249b522d0d1ced1f917ec7a0a81
hernandez87v <hernandez87v@hotmail.com>
2020-10-28 18:03:53 -0700

    add different pages and react router 🏗

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <h1>FIYA</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" rel="preconnect" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
