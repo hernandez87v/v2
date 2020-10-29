b8838a19ab167e9c7576c492fb5cda1d4bc98335
hernandez87v <hernandez87v@hotmail.com>
2020-10-29 14:24:37 -0700

    made textMesh its won component📦

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,25 +5,32 @@ import { Canvas } from 'react-three-fiber';
 import * as THREE from 'three';
 import Oswald from './Oswald.json';
 
-export default function Home() {
+function TextMesh(props) {
   // parse JSON file with Three
   const font = new THREE.FontLoader().parse(Oswald);
 
   // configure font geometry
   const textOptions = {
     font,
-    size: 2,
-    height: 1,
+    size: 3,
+    height: 0.5,
   };
+  return (
+    <mesh position={[-5, 0, 0]}>
+      <textGeometry attach="geometry" args={['Welcome', textOptions]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+}
+
+export default function Home() {
   return (
     <div className="Home">
       <Canvas
-        style={{ height: '100vh', width: '100vw' }} // stretch the canvas to the full viewport size
+        // style={{ height: '100vh', width: '100vw' }}
+        camera={{ position: [-10, 0, 30] }}
       >
-        <mesh>
-          <textGeometry attach="geometry" args={['Welcome', textOptions]} />
-          <meshStandardMaterial attach="material" />
-        </mesh>
+        <TextMesh />
       </Canvas>
     </div>
   );
