7f4e4fa90939194d6b6fd7ab82b0435b4cb3ae4b
hernandez87v <hernandez87v@hotmail.com>
2020-10-30 14:39:23 -0700

    several changes including adding texture 🏗 to letters

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,84 +1,83 @@
 import React, { useRef } from 'react';
 import './Home.css';
 
-import { Canvas, useFrame } from 'react-three-fiber';
+import { Canvas } from 'react-three-fiber';
 import * as THREE from 'three';
 import Modak from './Modak.json';
-// import texture from './space-texture.jpg';
 import { OrbitControls } from 'drei';
+import texture from './wavy-layers-black-paper-circles-background.jpg';
 
-const TextMesh = ({ args, position }) => {
+function TextMesh({ args, position }) {
   const mesh = useRef(null);
 
   const font = new THREE.FontLoader().parse(Modak);
-
+  const moon = new THREE.TextureLoader().load(texture);
+  moon.wrapS = THREE.RepeatWrapping;
+  moon.wrapT = THREE.RepeatWrapping;
+  moon.repeat.set(0.2, 0.2);
   const textOptions = {
     font,
     size: 4,
-    height: 1,
+    height: 2,
     curveSegments: 32,
     bevelEnabled: true,
     bevelThickness: 0.1,
     bevelSize: 0.1,
-    bevelOffset: 0,
-    bevelSegments: 1.5,
+    bevelOffset: 0.2,
+    bevelSegments: 0.1,
   };
-
   // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh castShadow position={position} ref={mesh}>
-      <textGeometry attach="geometry" args={[args, textOptions]} factor={0.7} />
-      <meshBasicMaterial attach="material" color="cyan" />
+    <mesh castShadow receiveShadow position={position} ref={mesh}>
+      <textGeometry attach="geometry" args={[args, textOptions]} factor={0.3} />
+      <meshBasicMaterial args={{ map: moon }} color="cyan" attach="material" />
       <OrbitControls />
     </mesh>
   );
-};
+}
+
+function Plane() {
+  const ref = useRef(null);
+
+  return (
+    <mesh
+      ref={ref}
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, -1, 0]}
+      receiveShadow
+    >
+      <planeBufferGeometry attach="geometry" args={[300, 300]} />
+      <shadowMaterial attach="material" color="#324c4a" opacity={0.3} />
+    </mesh>
+  );
+}
 
 export default function Home() {
-  function Plane(props) {
-    const ref = useRef(null);
-
-    return (
-      <mesh ref={ref} receiveShadow>
-        <planeBufferGeometry attach="geometry" args={[20, 20]} />
-        <shadowMaterial attach="material" color="#171717" opacity={0.3} />
-      </mesh>
-    );
-  }
-
   return (
     <div className="Home">
       <Canvas
         colorManagement
         shadowMap
-        camera={{ position: [0, 10, 75], fov: 75 }}
+        camera={{ position: [-20, 20, 75], fov: 40 }}
       >
-        <ambientLight intensity={0.3} />
-        <directionalLight
-          position={[0, 10, 0]}
-          intensity={1.5}
+        <ambientLight intensity={0.5} />
+        <spotLight
+          intensity={0.6}
+          position={[30, 30, 30]}
+          angle={0.3}
+          penumbra={1}
           castShadow
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
         />
-        <pointLight position={[-10, 0, -20]} intensity={0.5} />
-        <pointLight position={[0, -10, 0]} intensity={1.5} />
         <group>
-          <Plane rotation={[-Math.PI / 2, 0, 0]} />
-          <TextMesh args="W" position={[-16, 0, 0]} />
-          <TextMesh args="E" position={[-10, 0, 0]} />
-          <TextMesh args="L" position={[-5, 0, 0]} />
-          <TextMesh args="C" position={[0, 0, 0]} />
-          <TextMesh args="O" position={[5, 0, 0]} />
-          <TextMesh args="M" position={[10, 0, 0]} />
-          <TextMesh args="E" position={[15, 0, 0]} />
+          <Plane />
+          <TextMesh args="W" position={[-13, 0, 0.2]} />
+          <TextMesh args="E" position={[-7.5, 0, 0.2]} />
+          <TextMesh args="L" position={[-3.5, 0, 0.2]} />
+          <TextMesh args="C" position={[0, 0, 0.2]} />
+          <TextMesh args="O" position={[4, 0, 0.2]} />
+          <TextMesh args="M" position={[8, 0, 0.2]} />
+          <TextMesh args="E" position={[12.5, 0, 0.2]} />
         </group>
-        <gridHelper />
       </Canvas>
     </div>
   );
