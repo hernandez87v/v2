0cc59c1b8464762cc7d8e33c4716241ebdc4895b
hernandez87v <hernandez87v@hotmail.com>
2020-10-31 16:38:54 -0700

    playing with 🕸 meshPhysicalMaterial for texture on welcome text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ import Modak from './Modak.json';
 import { OrbitControls } from 'drei';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 
-function TextMesh({ props, args, position }) {
+function TextMesh({ args, position }) {
   const mesh = useRef(null);
 
   const font = new THREE.FontLoader().parse(Modak);
@@ -15,41 +15,21 @@ function TextMesh({ props, args, position }) {
   const textOptions = {
     font,
     size: 15,
-    height: 2,
-    curveSegments: 32,
+    height: 1.5,
+    curveSegments: 15,
     bevelEnabled: true,
     bevelThickness: 1.2,
     bevelSize: 1,
     bevelOffset: -0.1,
-    bevelSegments: 5,
+    bevelSegments: 4,
   };
   // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  useFrame(() => {
-    if (hovered && !active) {
-      mesh.current.rotation.y = 0.5;
-    }
-    if (hovered && active) {
-      mesh.current.rotation.y = 0.02;
-    }
-  });
   return (
-    <mesh
-      {...props}
-      scale={active ? [1.1, 1.1, 1.1] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-      castShadow
-      receiveShadow
-      position={position}
-      ref={mesh}
-    >
+    <mesh castShadow receiveShadow position={position} ref={mesh}>
       <textGeometry attach="geometry" args={[args, textOptions]} factor={0.3} />
-      <meshPhongMaterial
-        color={hovered ? 'cyan' : 'tomato'}
+      <meshPhysicalMaterial
+        roughness={0.2}
+        color={'tomato'}
         attach="material"
       />
       <OrbitControls enableZoom={false} />
@@ -63,12 +43,12 @@ function Plane() {
   return (
     <mesh
       ref={ref}
-      rotation={[-Math.PI / 30, 0, 0]}
-      position={[-20, -1.5, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, -10, 0]}
       receiveShadow
     >
       <planeBufferGeometry attach="geometry" args={[300, 300]} />
-      <shadowMaterial attach="material" color="white" opacity={0.3} />
+      <shadowMaterial attach="material" color="#383737" opacity={0.2} />
     </mesh>
   );
 }
@@ -94,32 +74,31 @@ export default function Home() {
             className="Home"
             colorManagement
             shadowMap
-            camera={{ position: [0, -20, 100], fov: 85 }}
+            camera={{ position: [0, 30, 100], fov: 85 }}
           >
             <ambientLight intensity={0.3} />
             <spotLight
-              intensity={0.3}
-              position={[150, 200, 50]}
+              intensity={0.5}
+              position={[190, 160, 60]}
               angle={0.3}
               penumbra={1}
               castShadow
             />
             <spotLight
-              intensity={0.4}
-              position={[80, 20, 40]}
-              angle={0.3}
+              intensity={0.2}
+              position={[-100, -50, -50]}
+              angle={0.2}
               penumbra={1}
-              castShadow
             />
             <group>
               <Plane />
-              <TextMesh args="W" position={[-35, 0, 1.2]} />
-              <TextMesh args="e" position={[-18, 0, 0.2]} />
-              <TextMesh args="l" position={[-6, 0, 1.2]} />
-              <TextMesh args="c" position={[0, 0, 0.2]} />
-              <TextMesh args="o" position={[12, 0, 1.2]} />
-              <TextMesh args="m" position={[23, 0, 0.2]} />
-              <TextMesh args="e" position={[40, 0, 1.2]} />
+              <TextMesh args="W" position={[-42, 0, 1.2]} />
+              <TextMesh args="e" position={[-25, 0, 0.2]} />
+              <TextMesh args="l" position={[-13, 0, 1.2]} />
+              <TextMesh args="c" position={[-7, 0, 0.2]} />
+              <TextMesh args="o" position={[3, 0, 1.2]} />
+              <TextMesh args="m" position={[14, 0, 0.2]} />
+              <TextMesh args="e" position={[31, 0, 1.2]} />
             </group>
           </Canvas>
         </div>
