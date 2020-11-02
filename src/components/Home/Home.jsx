2222a7cd2270be626c88eb51c4d5734518ac72dc
hernandez87v <hernandez87v@hotmail.com>
2020-11-01 16:14:15 -0800

    make adjustements to welcome screen🌟

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,8 @@ import Modak from './Modak.json';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 import { Physics, useBox, usePlane } from 'use-cannon';
 
-function TextMesh({ args = [0.5, 32, 32], ...props }) {
+// function TextMesh({ args = [0.5, 32, 32], ...props }) {
+function TextMesh({ args, position }) {
   const font = new THREE.FontLoader().parse(Modak);
 
   const textOptions = {
@@ -23,21 +24,22 @@ function TextMesh({ args = [0.5, 32, 32], ...props }) {
     bevelSegments: 4,
   };
 
-  const [ref] = useBox(() => ({
-    mass: 1,
-    position: [0, 0, 0],
-    angularDamping: 1,
-    // rotation: [0.4, 0.2, 0.5],
-    ...props,
-  }));
+  const ref = useRef(null);
+  // const [ref] = useBox(() => ({
+  //   mass: 1,
+  //   position: [0, 0, 0],
+  //   angularDamping: 1,
+  //   // rotation: [0.4, 0.2, 0.5],
+  //   ...props,
+  // }));
 
   return (
-    <mesh castShadow receiveShadow ref={ref}>
-      <textGeometry attach="geometry" args={[args, textOptions]} factor={0.3} />
+    <mesh position={position} castShadow receiveShadow ref={ref}>
+      <textGeometry attach="geometry" args={[args, textOptions]} factor={0.7} />
       <meshPhysicalMaterial
         clearcoat={1}
         roughness={0.2}
-        color="mediumspringgreen"
+        color="silver"
         attach="material"
       />
       {/* <OrbitControls enableZoom={false} /> */}
@@ -46,21 +48,21 @@ function TextMesh({ args = [0.5, 32, 32], ...props }) {
 }
 
 function Plane() {
-  // const ref = useRef(null);
-  const [ref, api] = usePlane(() => ({
-    position: [0, -1, 0],
-    rotation: [-Math.PI / 2, 0, 0],
-    onCollide: () => {
-      api.position.set(0, 0, 0);
-      api.velocity.set(0, 1, 0);
-    },
-  }));
+  const ref = useRef(null);
+  // const [ref, api] = usePlane(() => ({
+  //   position: [0, -1, 0],
+  //   rotation: [-Math.PI / 2, 0, 0],
+  //   onCollide: () => {
+  //     api.position.set(0, 0, 0);
+  //     api.velocity.set(0, 1, 0);
+  //   },
+  // }));
 
   return (
     <mesh
       ref={ref}
-      // rotation={[-Math.PI / -50, 0, 0]}
-      // position={[0, -25, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, 0, 0]}
       receiveShadow
     >
       <planeBufferGeometry attach="geometry" args={[300, 300]} />
@@ -68,6 +70,13 @@ function Plane() {
     </mesh>
   );
 }
+const url = (name, wrap = false) =>
+  `${
+    wrap ? 'url(' : ''
+  }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
+    wrap ? ')' : ''
+  }`;
+
 export default function Home() {
   // let parallax;
   // const ref = useRef(null);
@@ -77,13 +86,20 @@ export default function Home() {
       <ParallaxLayer
         offset={1}
         speed={1}
-        style={{ backgroundColor: '#805E73' }}
+        style={{ backgroundColor: '#5e7f79' }}
       />
       <ParallaxLayer
         offset={2}
         speed={1}
         style={{ backgroundColor: '#87BCDE' }}
       />
+      <ParallaxLayer
+        offset={0}
+        speed={0}
+        factor={3}
+        style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }}
+      />
+
       <ParallaxLayer>
         <div className="Home">
           <Canvas
@@ -106,16 +122,16 @@ export default function Home() {
               penumbra={1}
             />
             <group>
-              <Physics>
-                <Plane />
-                <TextMesh args="W" position={[-42, 0, 1.2]} />
-                <TextMesh args="e" position={[-25, 0, 0.2]} />
-                <TextMesh args="l" position={[-13, 0, 1.2]} />
+              {/* <Physics> */}
+              <Plane />
+              <TextMesh args="Welcome" position={[-42, 25, 2]} />
+              <TextMesh args={'My name is Vlad.'} position={[-75, 0, 0.2]} />
+              {/* <TextMesh args="l" position={[-13, 0, 1.2]} />
                 <TextMesh args="c" position={[-7, 0, 0.2]} />
                 <TextMesh args="o" position={[3, 0, 1.2]} />
                 <TextMesh args="m" position={[14, 0, 0.2]} />
-                <TextMesh args="e" position={[31, 0, 1.2]} />
-              </Physics>
+                <TextMesh args="e" position={[31, 0, 1.2]} /> */}
+              {/* </Physics> */}
             </group>
           </Canvas>
         </div>
