2c475e4c4595935666d708516cffebbe40136099
hernandez87v <hernandez87v@hotmail.com>
2020-11-05 16:26:20 -0800

    ☀ add sun and moon 🌑 depending on time of day

--- src/components/Home/Welcome/Welcome.jsx
+++ src/components/Home/Welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './Welcome.css';
 import { Canvas } from 'react-three-fiber';
 import { FontLoader } from 'three';
@@ -39,42 +39,69 @@ function TextMesh({ args, position }) {
         factor={0.7}
       />
       <meshPhysicalMaterial
-        clearcoat={1}
+        clearcoat={0}
         reflectivity={1}
-        roughness={0.4}
-        metalness={1}
-        color="tomato"
+        roughness={1}
+        // metalness={1}
+        color="lemonchiffon"
         attach="material"
       />
       <OrbitControls
         enableZoom={false}
         enabled={false}
         autoRotate
-        autoRotateSpeed={0.8}
+        autoRotateSpeed={0.3}
       />
     </mesh>
   );
 }
 
 export default function Welcome() {
+  // const date = new Date();
+  // const hour = date.getHours();
+  // this.setState({
+  //   hour,
+  // });
+
+  const date = new Date();
+  const [hour] = useState(date);
+  console.log(hour.getHours());
+
   return (
     <div className="Welcome">
       <Canvas colorManagement camera={{ position: [0, 30, 100], fov: 85 }}>
         <ambientLight intensity={0.2} />
-        <spotLight intensity={0.2} position={[30, -60, 160]} penumbra={1} />
-        <spotLight intensity={0.2} position={[-30, -10, 200]} penumbra={1} />
+        <spotLight intensity={0.2} position={[50, 80, 30]} penumbra={1} />
+        <spotLight intensity={0.2} position={[70, 30, 30]} penumbra={1} />
         <spotLight intensity={0.2} position={[-200, -150, -150]} penumbra={1} />
         <group>
-          <Planet args={[10, 32, 32]} position={[110, -25, 0]} color="silver" />
-          <Planet
-            args={[20, 32, 32]}
-            position={[0, 50, -100]}
-            color="lemonchiffon"
-          />
-          <TextMesh args="Hello," position={[-43, 20, 0]} />
-          <TextMesh args="my name" position={[-43, 0, 0]} />
-          <TextMesh args="is Vlad," position={[-43, -20, 0]} />
-          <TextMesh args="Web Dev." position={[-43, -40, 0]} />
+          {hour.getHours() < 18 ? (
+            <Planet
+              clearcoat={0}
+              reflectivity={0}
+              roughness={1}
+              args={[50, 32, 32]}
+              position={[0, 90, -310]}
+              color="orange"
+            />
+          ) : (
+            <Planet
+              clearcoat={0}
+              reflectivity={0}
+              roughness={1}
+              args={[10, 32, 32]}
+              position={[150, 50, 50]}
+              color="gray"
+            />
+          )}
+
+          {/* <TextMesh args="Vlad Hernandez" position={[-50, 0, 0]} />
+          <TextMesh args="Web Developer" position={[-43, -15, 0]} /> */}
+          <TextMesh args="Hello," position={[-43, 30, 0]} />
+          <TextMesh args="my name" position={[-43, 10, 0]} />
+          <TextMesh args="is Vlad," position={[-43, -10, 0]} />
+          <TextMesh args=">Web" position={[-43, -30, 0]} />
+          <TextMesh args="Developer." position={[-43, -50, 0]} />
           <Stars radius={75} count={1000} />
         </group>
       </Canvas>
