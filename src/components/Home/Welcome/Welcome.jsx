e17b2e258c9e0e60e096a7410c495a34e356ae81
hernandez87v <hernandez87v@hotmail.com>
2020-11-06 15:03:50 -0800

    removed background from welcome seen to just show home background

--- src/components/Home/Welcome/Welcome.jsx
+++ src/components/Home/Welcome/Welcome.jsx
@@ -6,7 +6,6 @@ import Modak from './Modak.json';
 import { OrbitControls, Stars, useTextureLoader } from 'drei';
 import sun_texture from './2kSun.jpg';
 import moon_texture from './2kMoon.jpg';
-import moonBump from './moonBump.jpg';
 
 function TextMesh({ args, position }) {
   const font = new FontLoader().parse(Modak);
@@ -38,26 +37,21 @@ function TextMesh({ args, position }) {
         attach="material"
       />
       <OrbitControls
-      // enableZoom={false}
-      // enabled={false}
-      // autoRotate
-      // autoRotateSpeed={0.3}
+        enableZoom={false}
+        enabled={false}
+        autoRotate
+        autoRotateSpeed={0.3}
       />
     </mesh>
   );
 }
 function Planet({ args, position, map, ...props }) {
   const ref = useRef();
-  const bumpMap = useTextureLoader(moonBump);
+
   return (
     <mesh position={position} {...props} ref={ref}>
       <sphereBufferGeometry attach="geometry" args={args} />
-      <meshStandardMaterial
-        bumpMap={bumpMap}
-        radius={1}
-        bumpScale={0.5}
-        map={map}
-      />
+      <meshStandardMaterial map={map} />
     </mesh>
   );
 }
@@ -66,37 +60,29 @@ export default function Welcome() {
   const date = new Date();
   const [hour] = useState(date);
   const [sun, moon] = useTextureLoader([sun_texture, moon_texture]);
-
-  // var c = <Canvas>...</Canvas>;
+  // var c = <Canvas></Canvas>;
   // console.log(c);
   // console.log(hour.getHours());
   return (
     <div className="Welcome">
-      <Canvas colorManagement camera={{ position: [0, 30, 100], fov: 85 }}>
+      <Canvas
+        alpha={false}
+        colorManagement
+        camera={{ position: [0, 30, 100], fov: 85 }}
+        gl={{
+          powerPreference: 'high-performance',
+        }}
+      >
         <ambientLight intensity={0.2} />
-        <spotLight intensity={0.2} position={[50, 80, 30]} penumbra={1} />
+        <spotLight intensity={0.3} position={[50, 30, 30]} penumbra={1} />
         <spotLight intensity={0.2} position={[70, 30, 30]} penumbra={1} />
         <spotLight intensity={0.2} position={[-200, -150, -150]} penumbra={1} />
         <group>
           <Suspense fallback={null}>
-            {hour.getHours() < 12 ? (
-              <Planet
-                clearcoat={0}
-                reflectivity={0}
-                roughness={1}
-                args={[150, 32, 32]}
-                position={[0, 90, -310]}
-                map={sun}
-              />
+            {hour.getHours() < 16 ? (
+              <Planet args={[50, 32, 32]} position={[0, 90, -310]} map={sun} />
             ) : (
-              <Planet
-                clearcoat={0}
-                reflectivity={0}
-                roughness={1}
-                args={[10, 32, 32]}
-                position={[150, 50, 50]}
-                map={moon}
-              />
+              <Planet args={[10, 32, 32]} position={[150, 50, 50]} map={moon} />
             )}
           </Suspense>
           <TextMesh args="Hello," position={[-43, 30, 0]} />
@@ -104,7 +90,6 @@ export default function Welcome() {
           <TextMesh args="is Vlad," position={[-43, -10, 0]} />
           <TextMesh args=">Web" position={[-43, -30, 0]} />
           <TextMesh args="Developer." position={[-43, -50, 0]} />
-          <Stars radius={150} count={1000} />
         </group>
       </Canvas>
     </div>
