11d67316f78aea393fdacbce7e5d92b4687afee6
hernandez87v <hernandez87v@hotmail.com>
2020-11-11 17:18:34 -0800

    add if else statement for time of day☀ greeting

--- src/components/Home/Welcome/Welcome.jsx
+++ src/components/Home/Welcome/Welcome.jsx
@@ -10,12 +10,12 @@ function TextMesh({ args, position }) {
   const textOptions = {
     font,
     size: 25,
-    height: 5,
+    height: 3,
     curveSegments: 15,
     bevelEnabled: true,
-    bevelThickness: 4,
+    bevelThickness: 2,
     bevelSize: 1,
-    bevelSegments: 2,
+    bevelSegments: 8,
   };
   const ref = useRef(null);
 
@@ -65,16 +65,24 @@ export default function Welcome() {
         <spotLight intensity={0.2} position={[0, 20, 200]} penumbra={1} />
         <spotLight intensity={0.2} position={[0, 0, 0]} penumbra={1} />
         <group>
-          <Suspense fallback={null}>
+          {/* <Suspense fallback={null}>
             {hour.getHours() < 16 ? (
               <Planet args={[50, 32, 32]} position={[-500, 400, -800]} />
             ) : (
               <Planet args={[10, 32, 32]} position={[150, 50, 50]} />
             )}
+          </Suspense> */}
+          <TextMesh args="Good" position={[-75, 60, 0]} />
+
+          <Suspense fallback={null}>
+            {hour.getHours() < 12 ? (
+              <TextMesh args="Morning," position={[-75, 30, 0]} />
+            ) : (
+              <TextMesh args="Afternoon," position={[-75, 30, 0]} />
+            )}
           </Suspense>
-          <TextMesh args="Hello," position={[-35, 30, 0]} />
-          <TextMesh args="my name" position={[0, 0, 0]} />
-          <TextMesh args="is Vlad," position={[-35, -35, 0]} />
+          <TextMesh args="my name" position={[-75, 0, 0]} />
+          <TextMesh args="is Vlad," position={[-75, -35, 0]} />
           {/* <TextMesh args=">Web" position={[-35, -20, -10]} />
           <TextMesh args="Developer." position={[-35, -30, 0]} /> */}
         </group>
