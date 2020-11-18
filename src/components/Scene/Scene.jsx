66271476271849b58a0eb9437429304df5be3497
hernandez87v <hernandez87v@hotmail.com>
2020-11-18 11:07:05 -0800

    removed unused JS and made all textMesh 1 component📦

--- src/components/Scene/Scene.jsx
+++ src/components/Scene/Scene.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { FontLoader } from 'three';
 import Modak from '../fonts/Modak.json';
-const font = new FontLoader().parse(Modak);
 
 export function Plane() {
   const ref = useRef(null);
@@ -14,6 +13,7 @@ export function Plane() {
 }
 
 export function TextMesh({ args, position }) {
+  const font = new FontLoader().parse(Modak);
   const textOptions = {
     font,
     size: 20,
@@ -24,8 +24,8 @@ export function TextMesh({ args, position }) {
     bevelSize: 0.9,
     bevelSegments: 5,
   };
-  const ref = useRef(null);
 
+  const ref = useRef(null);
   const [hovered, setHover] = useState(false);
 
   return (
@@ -67,36 +67,6 @@ export function Lighting() {
   );
 }
 
-export function WTextMesh({ args, position }) {
-  const textOptions = {
-    font,
-    size: 20,
-    height: 2,
-    curveSegments: 15,
-    bevelEnabled: true,
-    bevelThickness: 2,
-    bevelSize: 0.9,
-    bevelSegments: 5,
-  };
-  const ref = useRef(null);
-  return (
-    <mesh position={position} ref={ref} castShadow receiveShadow>
-      <textBufferGeometry
-        attach="geometry"
-        args={[args, textOptions]}
-        factor={0.7}
-      />
-      <meshPhysicalMaterial
-        clearcoat={1}
-        reflectivity={1}
-        roughness={0.3}
-        color="cyan"
-        attach="material"
-      />
-    </mesh>
-  );
-}
-
 export function WLighting() {
   return (
     <mesh>
@@ -121,20 +91,3 @@ export function WLighting() {
     </mesh>
   );
 }
-// function Box({ args, position, map, color, ...props }) {
-//   const ref = useRef();
-
-//   return (
-//     <mesh position={position} {...props} ref={ref}>
-//       <boxBufferGeometry attach="geometry" args={args} />
-//       {/* <meshStandardMaterial color={color} map={map} /> */}
-//       <MeshWobbleMaterial
-//         attach="material"
-//         color={color}
-//         // map={map}
-//         speed={2}
-//         factor={0.7}
-//       />
-//     </mesh>
-//   );
-// }
