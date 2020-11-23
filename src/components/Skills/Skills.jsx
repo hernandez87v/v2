a0d1186fbf55415c0cd701b37412070b30d41fc8
hernandez87v <hernandez87v@hotmail.com>
2020-11-22 20:52:10 -0800

    remove scripts for mesh and model from skills scene

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import './Skills.css';
 import { Canvas } from 'react-three-fiber';
 import { Lighting, Plane, TextMesh } from '../Scene/Scene';
-import Model from '../../Html';
+// import Model from '../../Html';
 
 export default function Skills() {
   return (
@@ -15,9 +15,9 @@ export default function Skills() {
         <Lighting />
         <group>
           <Plane />
-          <Suspense fallback={null}>
+          {/* <Suspense fallback={null}>
             <Model />
-          </Suspense>
+          </Suspense> */}
           <TextMesh args={'SKILLS'} position={[-50, 50, 0]} />
         </group>
       </Canvas>
