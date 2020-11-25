622ceaaae61c4eab43f9bb4f9e0b986c983383b7
hernandez87v <hernandez87v@hotmail.com>
2020-11-24 16:55:25 -0800

    add react.lazy to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Code from './components/Code/Code';
-import Home from './components/Home/Home';
-import Nav from './components/Nav/Nav';
-import Skills from './components/Skills/Skills';
+// import Home from './components/Home/Home';
+// import Code from './components/Code/Code';
+// import Nav from './components/Nav/Nav';
+// import Skills from './components/Skills/Skills';
+const Home = lazy(() => import('./components/Home/Home'));
+const Code = lazy(() => import('./components/Code/Code'));
+const Nav = lazy(() => import('./components/Nav/Nav'));
+const Skills = lazy(() => import('./components/Skills/Skills'));
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/" rel="preconnect" exact component={Home} />
-          <Route path="/skills" rel="preconnect" exact component={Skills} />
-          <Route path="/code" rel="preconnect" exact component={Code} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Nav />
+          <Switch>
+            <Route path="/" rel="preconnect" exact component={Home} />
+            <Route path="/skills" rel="preconnect" exact component={Skills} />
+            <Route path="/code" rel="preconnect" exact component={Code} />
+          </Switch>
+        </Suspense>
       </div>
     </Router>
   );
