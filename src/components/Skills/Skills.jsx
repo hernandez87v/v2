f1c2a5d118b6f1e10311c54a2ab6ae592a1bb043
hernandez87v <hernandez87v@hotmail.com>
2020-12-03 11:58:39 -0800

    remove glb files that were use for testing and added new logo.glb

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import './Skills.css';
 import { Canvas } from 'react-three-fiber';
 import { Lighting, Plane, TextMesh } from '../Scene/Scene';
-import Model from '../../ReactHtml';
+import Model from '../../AnimatedLogos';
 
 export default function Skills() {
   return (
