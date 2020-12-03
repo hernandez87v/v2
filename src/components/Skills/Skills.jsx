ecc6a270ed7217a578b80f70ac63ed784c63613c
hernandez87v <hernandez87v@hotmail.com>
2020-12-02 19:19:56 -0800

    add new ReactLogo model that is bigger in size and naming fixed

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import './Skills.css';
 import { Canvas } from 'react-three-fiber';
 import { Lighting, Plane, TextMesh } from '../Scene/Scene';
-import Model from '../../React';
+import Model from '../../React1';
 
 export default function Skills() {
   return (
