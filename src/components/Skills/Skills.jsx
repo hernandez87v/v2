fe325ab1da5a825c041fdc9fbadd180dd34de70c
hernandez87v <hernandez87v@hotmail.com>
2020-12-03 11:26:29 -0800

    add different updated glb files modelled in blender to see the outcome in the skills page

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import './Skills.css';
 import { Canvas } from 'react-three-fiber';
 import { Lighting, Plane, TextMesh } from '../Scene/Scene';
-import Model from '../../React1';
+import Model from '../../ReactHtml';
 
 export default function Skills() {
   return (
