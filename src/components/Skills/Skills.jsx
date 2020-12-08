ac2fd91a25086e4707d2605544c1527ed892175d
hernandez87v <hernandez87v@hotmail.com>
2020-12-07 20:03:33 -0800

    remove Canvas scene 🎬 from Skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import './Skills.css';
 import { Canvas } from 'react-three-fiber';
-import { Lighting, Plane, TextMesh, Canvas } from '../Scene/Scene';
+import { Lighting, Plane, TextMesh } from '../Scene/Scene';
 import Model from '../../Logos';
 
 export default function Skills() {
