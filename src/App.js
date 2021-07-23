ce6d6a11259e45f23174573d833591aa9de39ba9
hernandez87v <hernandez87v@hotmail.com>
2021-07-23 14:20:13 -0700

    add target and rel to a tag to open new tab when links clicked/ also added IG

--- src/App.js
+++ src/App.js
@@ -32,16 +32,29 @@ function App() {
         href="https://github.com/hernandez87v"
         className="top-left"
         children="Github"
+        target="_blank"
+        rel="noopener noreferrer"
       />
       <a
         href="https://www.linkedin.com/in/vlad-hernandez/"
         className="top-middle"
         children="LinkedIn"
+        target="_blank"
+        rel="noopener noreferrer"
       />
       <a
         href="https://twitter.com/vancity_exe"
         className="top-right"
         children="Twitter"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+      <a
+        href="https://www.instagram.com/vancity.exe"
+        className="top-right"
+        children="Instagram"
+        target="_blank"
+        rel="noopener noreferrer"
       />
     </div>
   );
